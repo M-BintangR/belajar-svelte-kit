c1a1613bc4126c9bf8bead2aef5cadf11fe7e1dc
= <muhbintang650@gmail.com>
2023-12-20 13:27:55 +0800

    navigating programmatically ✅

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
+<script>
+	import {goto, beforeNavigate, afterNavigate} from "$app/navigation";
+	
+	const handleClick = () => {
+		goto('/product');
+	}
+
+	beforeNavigate(navigate => {
+		console.log({before: navigate});
+	});
+
+	afterNavigate(navigate => {
+		console.log({after: navigate});
+	});
+
+</script>
 <main>
 	<h1>Welcome to SvelteKit</h1>
 	<a href="/blog">Blog</a>
 	<a href="/product">Product</a>
+
+	<button on:click={handleClick}>Place order</button>
 </main>
