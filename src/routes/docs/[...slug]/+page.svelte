f76aec3abed3b248efcda81b1fc3c14e9f092052
= <muhbintang650@gmail.com>
2023-12-20 09:45:09 +0800

    catch all routes ✅

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from '$app/stores';
+
+    const slugArr = $page.params.slug.split("/");
+</script>
+
+<h1>Docs home page</h1>
+
+{#if slugArr.length == 1}
+    <h2>Viewing docs for features {slugArr[0]}</h2>
+{:else if slugArr.length == 2}
+    <h2>Viewing docs for feature {slugArr[0]} and concept {slugArr[1]}</h2>
+{/if}
