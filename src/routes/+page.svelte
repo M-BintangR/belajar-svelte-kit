de11a17edf383dbdca5dd2cd4c6a863a7c8f25ab
= <muhbintang650@gmail.com>
2023-12-20 13:10:05 +0800

    route navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <main>
 	<h1>Welcome to SvelteKit</h1>
+	<a href="/blog">Blog</a>
+	<a href="/product">Product</a>
 </main>
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -1,4 +1,10 @@
+<script>
+    let productId = 100;
+</script>
+<a href="/">Home</a>
+
 <h1>Products</h1>
-<h2>Product 1</h2>
-<h2>Product 2</h2>
-<h2>Product 3</h2>
+<h2><a href="/product/1">Product 1</a></h2>
+<h2><a href="/product/2">Product 2</a></h2>
+<h2><a href="/product/3">Product 3</a></h2>
+<h2><a href="/product/{ productId }">Product {productId}</a></h2>
