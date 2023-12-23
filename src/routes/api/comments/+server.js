0e9b1c5384a7617c078478e4254f418bfd520217
= <muhbintang650@gmail.com>
2023-12-23 09:24:14 +0800

    api post request ✅

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -4,3 +4,14 @@ import { json } from '@sveltejs/kit';
 export function GET(){
     return json(comments);
 }
+
+export async function POST(requestEvent){
+    const {request} = requestEvent;
+    const {text} = await request.json()
+    const newComments = {
+        id: comments.length + 1,
+        text,
+    };
+    comments.push(newComments);
+    return json(newComments, {status: 201});
+}
